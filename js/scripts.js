0a66ebf2ba1c3e5cc09f6893def3c079425a2511
Thanh Trong Vo <votrong1471@gmail.com>
2024-03-06 22:38:26 +0700

    add

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// Leaflet Map
+
+const map = L.map('map').setView([53.342686, -6.267118], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
